b1dba6621ae20e962ec8f6345e12ab24b8d4e4fe
Rupeshpharande <pharanderupesh@gmail.com>
2024-10-17 17:34:45 +0530

    Update Freaquency.cs

--- ForLoop/Freaquency.cs
+++ ForLoop/Freaquency.cs
@@ -8,32 +8,10 @@ namespace ForLoop
 {
     internal class Freaquency
     {
-        //static void Main(string[] args)
-        //{
-        //    int count = 0;
-        //    string str = (Console.ReadLine());
-
-
-
-        //    for (int i = 1; i < str.Length - 1; i++)
-        //    {
-        //        char ch = str[i];
-        //        for (int j = i + 1; j < str.Length; j++)
-        //        {
-        //            if (ch == str[j])
-        //            {
-        //                count++;
-        //            }
-
-        //        }
-        //        if (count >= 1)
-        //        {
-        //            Console.WriteLine($"{count}");
-        //        }
-
-
-        //    }
-        //}
+        static void Main(string[] args)
+        {
+       
+        }
 
 
 
